57149db7bbb5853585ffee1378b9f0d22738298a
suraksh <surakshnss@gmail.com>
2020-05-26 03:10:09 +0530

    sd

--- preetham1/script.js
+++ preetham1/script.js
@@ -4,7 +4,7 @@ const happy = document.querySelector('#happy');
 const body = document.querySelector('body');
 const h1 = document.querySelector('h1');
 const music = document.querySelector('#music');
-const scrollAmount = 40;
+const scrollAmount = 55;
 window.addEventListener("scroll",(event) => {
 	const { top } = image.getBoundingClientRect();
 	if (top < scrollAmount) {
